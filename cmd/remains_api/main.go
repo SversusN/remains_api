4b88bd7999c3125b41a248ef4ee6ed026d3075e7
vg_varankin <skaldnord@yandex.ru>
2024-06-29 16:08:38 +0300

    gracefull

--- cmd/remains_api/main.go
+++ cmd/remains_api/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"remains_api/internal/app"
+	"syscall"
+	"time"
 )
 
-// @title chi-swagger example APIs
-// @version 1.0
-// @description chi-swagger example APIs
-// @BasePath /
 func main() {
 	a := app.NewApp()
 	r := a.CreateRouter()
+	server := &http.Server{Addr: a.C.Port, Handler: r}
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
 
-	err := http.ListenAndServe(a.C.Port, r)
-	if err != nil {
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if errors.Is(shutdownCtx.Err(), context.DeadlineExceeded) {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+	err := server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
+	<-serverCtx.Done()
 }
